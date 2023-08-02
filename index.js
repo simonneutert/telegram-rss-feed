81b0f07c80eaf466e2d1a273fc3aca2574f99b29
Simon Neutert <simonneutert@users.noreply.github.com>
2023-08-02 17:45:44 +0200

    add a test pipeline and refactor the project as I go (#32)
    * init
    
    * lint, lint:fix and test with mocha 🥰

--- index.js
+++ index.js
@@ -1,77 +1,16 @@
-const { Sequelize, DataTypes } = require('sequelize');
+const { Db } = require('./lib/db');
+const { parseFeed } = require('./lib/parseFeed');
+const { sendMessage } = require('./lib/sendMessage');
 
-const sequelize = new Sequelize({
-  // The `host` parameter is required for other databases
-  // host: 'localhost'
-  dialect: 'sqlite',
-  storage: './db/database.sqlite',
-});
-
-/**
- * config your database setup please check the details in the parser
- * on how to link you rss items and write them into the database.
- * I chose Article, because that's what an RSS Item typically is.
- */
-const Article = sequelize.define('Article', {
-  title: DataTypes.STRING,
-  content: DataTypes.TEXT,
-  contentSnippet: DataTypes.TEXT,
-  creator: DataTypes.STRING,
-  guid: {
-    type: DataTypes.STRING,
-  },
-  link: {
-    type: DataTypes.STRING,
-    unique: true,
-  },
-});
-
-sequelize
-  .sync() // { force: true } will be useful if you need to start from scratch
+Db.sync() // { force: true } will be useful if you need to start from scratch
   .then(() => {});
-
-const { Telegraf } = require('telegraf');
-const bot = new Telegraf(process.env.TELEGRAM_API_KEY);
-const Parser = require('rss-parser');
-const parser = new Parser();
 
 /**
  * this function auto calls itself in an async fashion
  */
 (async () => {
-  const feed = await parser.parseURL(process.env.FEED_URL);
-  console.log(feed.title);
-
-  feed.items.forEach((item) => {
-    // this is where the items are written to the db
-    // please, read this and edit to fit.
-    Article.create({
-      title: item.title,
-      content: item.content,
-      contentSnippet: item.contentSnippet,
-      creator: item.creator,
-      guid: item.guid,
-      link: item.link,
-    })
-      .then((persistedItem) => {
-        // process.env.TELEGRAM_CHANNEL_OR_GROUP should be a string like:
-        // @this_is_my_TELEGRAM_CHANNEL_OR_GROUP
-        //
-        bot.telegram
-          .sendMessage(
-            process.env.TELEGRAM_CHANNEL_OR_GROUP,
-            persistedItem.link,
-          )
-          .then((msg) => {
-            console.log(msg);
-            console.log(persistedItem.link);
-          })
-          .catch((e) => {
-            console.log(e);
-          });
-      })
-      .catch((e) => {
-        console.error(e);
-      });
+  await parseFeed(process.env.FEED_URL, sendMessage, {
+    telegramApiKey: process.env.TELEGRAM_API_KEY,
+    telegramChannelOrGroup: process.env.TELEGRAM_CHANNEL_OR_GROUP,
   });
 })();
---
+++ test/test.js
@@ -0,0 +1,78 @@
+const assert = require('assert');
+const { DataTypes } = require('sequelize');
+
+const { Db } = require('../lib/db');
+const { parseFeed } = require('../lib/parseFeed');
+const { sendMessage } = require('../lib/sendMessage');
+const { articleDefinition } = require('../lib/createArticleSchema');
+
+const url = 'https://www.nasa.gov/rss/dyn/lg_image_of_the_day.rss';
+
+Db.sync({ force: true }) // { force: true } will be useful if you need to start from scratch
+  .then(() => {});
+
+describe('main.js', function () {
+  describe('ArticleSchema definition', function () {
+    it('should be defined with certain properties', function () {
+      assert(articleDefinition.title);
+      assert.equal(articleDefinition.title, DataTypes.STRING);
+      assert(articleDefinition.content);
+      assert.equal(articleDefinition.content, DataTypes.TEXT);
+      assert(articleDefinition.contentSnippet);
+      assert.equal(articleDefinition.contentSnippet, DataTypes.TEXT);
+      assert(articleDefinition.creator);
+      assert.equal(articleDefinition.creator, DataTypes.STRING);
+
+      assert.equal(articleDefinition.guid.type.key, 'STRING');
+      assert(articleDefinition.link);
+      assert.equal(articleDefinition.link.type.key, 'STRING');
+      assert(articleDefinition.link.unique);
+    });
+  });
+
+  describe('sendMessage function', function () {
+    it('should return false if no persistedItem is passed', function () {
+      const result = sendMessage();
+      assert.equal(result, false);
+    });
+    it('should return false if no link is passed', function () {
+      const result = sendMessage({});
+      assert.equal(result, false);
+    });
+    it('should return false if no options.telegramApiKey is passed', function () {
+      const result = sendMessage({
+        link: 'http://example.com',
+        telegramChannelOrGroup: '1234567890',
+      });
+      assert.equal(result, false);
+    });
+    it('should return false if no options.telegramChannelOrGroup is passed', function () {
+      const result = sendMessage({
+        link: 'http://example.com',
+        telegramApiKey: '1234567890',
+      });
+      assert.equal(result, false);
+    });
+  });
+
+  it('parse feed and return articles', async function () {
+    const result = await parseFeed(url, sendMessage, []);
+    assert(result.length);
+    Promise.all(result).then((values) => {
+      const firstResult = values[0];
+      assert(firstResult.title);
+      assert(typeof firstResult.title === 'string');
+      assert(firstResult.link);
+      assert(typeof firstResult.link === 'string');
+      assert(firstResult.link.includes('http'));
+      assert(firstResult.content);
+      assert(typeof firstResult.content === 'string');
+      assert(firstResult.contentSnippet);
+      assert(typeof firstResult.contentSnippet === 'string');
+      assert(firstResult.creator);
+      assert(typeof firstResult.creator === 'string');
+      assert(firstResult.guid);
+      assert(typeof firstResult.guid === 'string');
+    });
+  });
+});
