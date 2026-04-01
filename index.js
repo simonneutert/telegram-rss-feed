62978c0406449711546c0c98ea28cb334a3a11f2
Simon Neutert <simonneutert@users.noreply.github.com>
2026-04-01 21:16:39 +0200

    Add Zod validation for environment variables and feed items

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
+const { z } = require('zod');
 const { db } = require('./lib/db');
 const { createArticleSchema } = require('./lib/createArticleSchema');
 const { parseFeed } = require('./lib/parseFeed');
 const { sendMessage } = require('./lib/sendMessage');
 
+const EnvSchema = z.object({
+  FEED_URL: z.url(),
+  TELEGRAM_API_KEY: z.string().min(1),
+  TELEGRAM_CHANNEL_OR_GROUP: z.string().min(1),
+});
+
+const env = EnvSchema.parse(process.env);
+
 createArticleSchema(db);
 
 (async () => {
-  await parseFeed(process.env.FEED_URL, sendMessage, {
-    telegramApiKey: process.env.TELEGRAM_API_KEY,
-    telegramChannelOrGroup: process.env.TELEGRAM_CHANNEL_OR_GROUP,
+  await parseFeed(env.FEED_URL, sendMessage, {
+    telegramApiKey: env.TELEGRAM_API_KEY,
+    telegramChannelOrGroup: env.TELEGRAM_CHANNEL_OR_GROUP,
   });
 })();
